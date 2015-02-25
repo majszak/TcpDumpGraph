09d159daaf6d5630b73ae7a0c171a0fae346e2d6
majszak <mm@psti.pl>
2015-02-25 01:02:02 +0100

    First version

--- src/main/groovy/tcpdump_graph.groovy
+++ src/main/groovy/tcpdump_graph.groovy
@@ -1,3 +1,4 @@
+import java.time.LocalTime
 import java.util.stream.Collectors
 
 import static java.nio.file.Files.lines
@@ -8,25 +9,31 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.Paths
 import java.util.function.Predicate
 
-println("EX")
+final String TCPDUMP_INPUT_FILE = '..\\resources\\dump.txt'
+final String FILTER = "IP 172.16.1.1"
+final int TIME_SEGMENT_LENGTH = 8
+final String LENGTH_REGEXP = /length (.*)/
 
+println("Start")
 
-Predicate<String> predicate = new Predicate<String>() {
+def predicate = new Predicate<String>() {
     @Override
     boolean test(String t) {
-        t.contains("size");
+        t.contains(FILTER)
     }
 }
-
-String f = '..\\resources\\dump.txt'
 
 CharsetDecoder dec = StandardCharsets.UTF_8.newDecoder()
         .onMalformedInput(CodingErrorAction.IGNORE)
 
-//.each( { i -> print i } )
+List<String> filtered = lines(Paths.get(TCPDUMP_INPUT_FILE), dec.charset()).filter(predicate).collect(Collectors.toList());
+Map<LocalTime, String> map = [:]
+filtered.each {def timeTokens = it.substring(0, TIME_SEGMENT_LENGTH).tokenize(':');
 
-//List<String> filtered = lines(Paths.get(f), dec.charset()).filter(predicate).collect(Collectors.toList());
+    String matcher = ( it =~ LENGTH_REGEXP )
+    println it;
+    map.put(new LocalTime(timeTokens.get(0).toInteger(), timeTokens.get(1).toInteger(), timeTokens.get(2).toInteger(), 0), matcher[0])}
 
-HashMap<String, String>[] array =
-        lines(Paths.get(f), dec.charset()).filter(predicate).toArray( { n -> print n; new HashMap[n] })
-print array
+//print filtered.size()
+println map
+println map.size()
